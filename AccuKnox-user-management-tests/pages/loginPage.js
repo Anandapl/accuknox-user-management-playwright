35788924163d13bba879aa5d494bfa7d5fd6f9d8
Anandapl <101395277+Anandapl@users.noreply.github.com>
2025-11-27 22:50:53 +0530

    Update loginPage.js

--- AccuKnox-user-management-tests/pages/loginPage.js
+++ AccuKnox-user-management-tests/pages/loginPage.js
@@ -1,5 +1,5 @@
 class LoginPage {
-  constructor(page) {
+  constructor(page) {   
     this.page = page;
     this.username = page.locator("input[name='username']");
     this.password = page.locator("input[name='password']");
