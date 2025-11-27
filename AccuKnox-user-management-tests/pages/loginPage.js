1b6b27d5a7dc2be38334cd8bd54ff7952308ef40
Anandapl <101395277+Anandapl@users.noreply.github.com>
2025-11-27 22:56:50 +0530

    Update loginPage.js

--- AccuKnox-user-management-tests/pages/loginPage.js
+++ AccuKnox-user-management-tests/pages/loginPage.js
@@ -1,11 +1,11 @@
 class LoginPage {
-  constructor(page) {       
+  constructor(page) {
     this.page = page;
     this.username = page.locator("input[name='username']");
     this.password = page.locator("input[name='password']");
     this.loginBtn = page.locator("button[type='submit']");
   }
-     
+  
   async login(username, password) {
     await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     await this.page.waitForSelector("input[name='username']");
