9c73aff49dba5578ff68b67a5048cdb21d6e5676
Anandapl <101395277+Anandapl@users.noreply.github.com>
2025-11-27 23:24:45 +0530

    Update adminPage.js

--- AccuKnox-user-management-tests/pages/adminPage.js
+++ AccuKnox-user-management-tests/pages/adminPage.js
@@ -1,7 +1,7 @@
 const { expect } = require("@playwright/test");
 
-class AdminPage {   
-  constructor(page) {     
+class AdminPage {
+  constructor(page) {
     this.page = page;
 
     // Navigation
